c020d724c4ffe67ec74610b355a95769e8d78384
MURILO RIBEIRO BARBOSA CAMELO <murilo.camelo@senaisp.edu.br>
2026-09-04 09:34:16 -0300

    Esse projeto é do Senai

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$nome = "Murilo Ribeiro Barbosa Camelo";
+$profissao = "Aprendiz de Desenvolvimneto de Sistemas";
+$mensagem = "Esse é meu centésimo projeto Usando GIT";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto PHP - Murilo Ribeiro Barbosa Camelo</title>
+</head>
+<body>
+    <h1><?php echo $nome; ?></h1>
+    <h1><?php echo $profissao; ?></h1>
+    <h1><?php echo $mensagem; ?></h1>
+</body>
+</html>
